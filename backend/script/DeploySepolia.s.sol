066e52364eae67e84830ad19a892d1cae3b9fdd7
benyzzz <133372786+benyzzz@users.noreply.github.com>
2025-07-20 19:01:50 +0200

    Add console import in DeploySepolia script

--- backend/script/DeploySepolia.s.sol
+++ backend/script/DeploySepolia.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 import "../src/Swap.sol";
 import "../src/EldoradoToken.sol";
 import "../src/MockERC20.sol";
